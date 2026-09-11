9bbada5fedf85d18c21fbbfbdbf047347dc47456
Ilia Colombini <ilia.colombini@gmail.com>
2026-09-11 12:43:32 +0200

    Play non funziona

--- sw.js
+++ sw.js
@@ -1,12 +1,16 @@
-const CACHE = 'carloquest-v1';
-const FILES = ['/', '/game/', '/game/index.html', '/game/manifest.json', '/game/icon.svg'];
+const CACHE = 'carloquest-v2';
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+  // Don't pre-cache anything, just activate immediately
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
+  );
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
-  );
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
